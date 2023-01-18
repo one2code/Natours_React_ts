b0dfeac8942404656ad3a30c5ff5dfd706e6bddf
Travis Bennett <t_bennett_a@protonmail.com>
2023-01-17 23:56:35 -0500

    Updated header component

--- natours/src/components/Header.tsx
+++ natours/src/components/Header.tsx
@@ -1,10 +1,16 @@
-import Logo from '../components/logo/Logo'
+import Logo from "../components/logo/Logo";
 
 function Header() {
-  return (
-    <div className = 'header'>
-        <Logo/>
-    </div>
-  )
+	return (
+		<div className="container">
+			<div className="header">
+				<Logo />
+				<h1 className="heading-primary">
+					<span className="heading-primary--main">Outdoors</span>
+					<span className="heading-primary--sub">is where life happens</span>
+				</h1>
+			</div>
+		</div>
+	);
 }
-export default Header
+export default Header;
